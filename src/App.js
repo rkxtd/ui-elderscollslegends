3e6a41d5427a23efc5f51868a1004a0dbfb17d72
Roman Shuvaryk <rshuvaryk@gmail.com>
2020-10-15 18:19:12 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Hooray! Elder Scroll Database is Up and Running!</h1>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("root"));
