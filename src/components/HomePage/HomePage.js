9d1b69fde3bd4104b9ed7dacd1c1a77da0067816
Roman Shuvaryk <rshuvaryk@gmail.com>
2020-10-16 13:27:12 -0700

    Added Spinner. Added Infinity Scroll to Grid.

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import PictureCard from "../../components/PictureCard";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import PictureCard from "../PictureCard";
+import Spinner from "../Spinner";
 import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
@@ -17,50 +19,73 @@ const useStyles = makeStyles((theme) => ({
   },
   control: {
     padding: theme.spacing(2),
-  },
+  }
 }));
 
+async function loadMoreData(setCards, setPage, page) {
+  const pageSize = 20;
+  const {
+    data: { cards },
+  } = await axios(
+    `https://api.elderscrollslegends.io/v1/cards?pageSize=${pageSize}&page=${page}`
+  );
+  setCards(oldCards => [...oldCards, ...cards]);
+  setPage(page + 1);
+}
 export default function HomePage() {
   const classes = useStyles();
   const [cards, setCards] = useState([]);
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    const loadData = async () => {
-      const {
-        data: { cards },
-      } = await axios(
-        "https://api.elderscrollslegends.io/v1/cards?pageSize=20"
-      );
-      setCards(cards);
-    };
-    loadData();
+    loadMoreData(setCards, setPage, page);
   }, []);
+
+  const fetchMoreData = async () => {
+    return await loadMoreData(setCards, setPage, page);
+  }
   return (
-    <Grid container className={classes.root} spacing={2}>
-      <Grid item xs={12}>
-        <Grid container justify="center" spacing={2}>
-          {cards.map(
-            ({
-              id,
-              imageUrl,
-              name: cardName,
-              type: cardType,
-              text: cardText,
-              set: { name: setName },
-            }) => (
-              <Grid key={id} item>
-                <PictureCard
-                  className={classes.paper}
-                  imageUrl={imageUrl}
-                  cardName={cardName}
-                  cardText={cardText}
-                  cardType={cardType}
-                  setName={setName}
-                />
+          <InfiniteScroll
+            dataLength={cards.length}
+            next={fetchMoreData}
+            hasMore={true}
+            loader={<Spinner />}
+            height={'100vh'}
+            endMessage={
+              <p style={{ textAlign: "center" }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }
+          >
+            <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12}>
+              <Grid container justify="center" spacing={2}>
+            {cards.map(
+              ({
+                 id,
+                 imageUrl,
+                 name: cardName,
+                 type: cardType,
+                 text: cardText,
+                 set: { name: setName },
+               }) => (
+                <Grid key={id} item>
+                  <PictureCard
+                    className={classes.paper}
+                    imageUrl={imageUrl}
+                    cardName={cardName}
+                    cardText={cardText}
+                    cardType={cardType}
+                    setName={setName}
+                  />
+                </Grid>
+              )
+            )}
               </Grid>
-            )
-          )}
-        </Grid>
-      </Grid>
-    </Grid>
+            </Grid>
+          </Grid>
+
+          </InfiniteScroll>
+
   );
 }
