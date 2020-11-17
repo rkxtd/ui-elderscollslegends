c1b6c02dd40df4d3a63d0ff1632b46f7b5871c13
Roman Shuvaryk <rshuvaryk@gmail.com>
2020-11-17 11:00:53 -0800

    Added button to clear search. Fixed mobile view title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
+import Button from "@material-ui/core/Button";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
 import { Context, SEARCH } from "../../stores/Cards";
@@ -11,14 +12,24 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+  appBar: {
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: 'column',
+    },
+  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
-    display: "none",
     [theme.breakpoints.up("sm")]: {
       display: "block",
+    },
+  },
+  searchWrapper: {
+    display: 'flex',
+    [theme.breakpoints.down("xs")]: {
+      paddingBottom: 10,
     },
   },
   search: {
@@ -29,6 +40,7 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginLeft: 0,
+    marginRight: 10,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
@@ -65,6 +77,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Header() {
   const classes = useStyles();
   const { dispatch } = useContext(Context);
+  const inputRef = useRef(null);
 
   const handleEnter = (event) => {
     const { value } = event.target;
@@ -82,27 +95,42 @@ export default function Header() {
       dispatch({ type: SEARCH, searchTerm, creatureType });
     }
   };
+
+  const handleClearButtonClick = () => {
+    inputRef.current.value = '';
+    dispatch({ type: SEARCH });
+  }
+
   return (
     <div className={classes.root}>
-      <AppBar position="fixed">
-        <Toolbar>
+      <AppBar position="fixed" >
+        <Toolbar className={classes.appBar}>
           <Typography className={classes.title} variant="h6" noWrap>
             Elder Scrolls: Legends
           </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
+          <div className={classes.searchWrapper}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                inputRef={inputRef}
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ "aria-label": "search" }}
+                onKeyDown={handleEnter}
+              />
+
             </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ "aria-label": "search" }}
-              onKeyDown={handleEnter}
-            />
+            <Button
+              variant="contained"
+              onClick={handleClearButtonClick}
+            >Clear</Button>
           </div>
+
         </Toolbar>
       </AppBar>
     </div>
